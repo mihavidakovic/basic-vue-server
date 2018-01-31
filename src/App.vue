e6ed1150e1138767ecd57483e9a4999ddc268cb7
Miha Vidakovič <miha@vidakovic.si>
2018-01-31 01:44:39 +0100

    removed Counter component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
 	<div id="app">
-		<Counter></Counter>
 	</div>
 </template>
 
 <script>
 	
-	import Counter from '@/app/Counter'
 	export default {
 		name: 'app',
 		components: {
-			Counter
 		}
 	}
 </script>
